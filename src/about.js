2ae6e6c376e6deeda2e0f4c4b670fed5b22b7f11
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-06 09:56:36 +0100

    Create module for 'about' page content

---
+++ src/about.js
@@ -0,0 +1,8 @@
+export default function loadAbout() {
+  const content = document.querySelector("#content");
+
+  content.innerHTML = `
+        <div class="hero">
+            <h1>About us</h1>
+        </div>`;
+}
