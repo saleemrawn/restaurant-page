5c3df138acaacaa291dfdd7544d31440b5e2dbdf
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-06 09:55:27 +0100

    Create module for 'menu' page content

---
+++ src/menu.js
@@ -0,0 +1,8 @@
+export default function loadMenu() {
+  const content = document.querySelector("#content");
+
+  content.innerHTML = `
+        <div class="hero">
+            <h1>Our menu</h1>
+        </div>`;
+}
