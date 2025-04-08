7497c688f099d5ecb9ae44da50253ec5f9c680d0
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-08 11:26:34 +0100

    Update class name for hero container

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ export default function loadAbout() {
   const content = document.querySelector("#content");
 
   content.innerHTML = `
-        <div class="hero">
+        <div class="about-hero">
             <h1>About us</h1>
         </div>`;
 }
